0e7990e94ea2f81de0dcd97941d9dc19fc8aa7cd
Ahmad Tamer Samir Qazamel <ahmad.qazamel@bld.ai>
2022-11-02 20:35:38 +0200

    Backend/ Create "Album" Model
    -Add the following Columns:
        - "album_id", "album_name", "album_release_date", and "cost".
        - "artist", which references the Artist table and forms a OneToMany Rel.
        - "date_created", saves the date this instance was created.

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from artists.models import Artist
+
+
+class Album(models.Model):
+    album_id = models.AutoField(primary_key=True)
+
+    album_name = models.CharField(max_length=500, default="New Album")
+
+    artist = models.ForeignKey(Artist,null=False, blank=False, related_name="albums",
+                               on_delete=models.CASCADE)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    album_release_date = models.DateTimeField(null=False, blank=False, default=timezone.now)
+
+    cost = models.DecimalField(max_digits=5, decimal_places=2, blank=False, null=False)
+
+    class Meta:
+        db_table = "Albums"
