685bb43e704c0335037c5bff540f7e4730d5acbe
Ahmad Tamer Samir Qazamel <ahmad.qazamel@bld.ai>
2022-11-02 19:11:00 +0200

    Create Artist Model
    - Add "artist_id" column as a primary key
    - Add "stage_name" column and make it unique
    - Add "social_media_link" column as a URLField
    - Order by "stage_name"

--- artists/models.py
+++ artists/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    artist_id = models.IntegerField(primary_key=True)
+    stage_name = models.CharField(max_length=50, required=True, unique=True)
+    social_media_link = models.URLField(max_length=200, null=False, blank=True)
+
+    class Meta:
+        db_table = "Artists"
+        ordering = ["stage_name"]
