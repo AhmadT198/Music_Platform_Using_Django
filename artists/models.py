9947c3de7352dd899cb06d55645e4540de051782
Ahmad Tamer Samir Qazamel <ahmad.qazamel@bld.ai>
2022-11-02 20:36:52 +0200

    Backend/ Create "Artist" Model
    -Add the following Columns:
        - "album_id", "stage_name", and "social_media_link".
    -Order the table by the "stage_name".
    -Add the apps to INSTALLED_APPS

--- artists/models.py
+++ artists/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 
 class Artist(models.Model):
-    artist_id = models.IntegerField(primary_key=True)
-    stage_name = models.CharField(max_length=50, required=True, unique=True)
+    artist_id = models.AutoField(primary_key=True)
+
+    stage_name = models.CharField(max_length=50, blank=False, null=False, unique=True)
+
     social_media_link = models.URLField(max_length=200, null=False, blank=True)
 
     class Meta:
