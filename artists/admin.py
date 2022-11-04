324316f77ef090b8100f912b79076cc73cda2406
Ahmad Tamer Samir Qazamel <ahmad.qazamel@bld.ai>
2022-11-04 13:40:01 +0200

    Backend/Allow the admin to create Albums from the Artist's editing form

--- artists/admin.py
+++ artists/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from artists.models import Artist
 from artists.forms import ArtistForm
+from albums.models import Album
 
 
 # Register your models here.
+class AlbumTabular(admin.TabularInline):
+    model = Album
 
 @admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
     form = ArtistForm
     list_display = ['stage_name', 'social_media_link', 'approved_albums_count']
-
+    inlines = [AlbumTabular]
     def approved_albums_count(self, obj):
         return obj.albums.filter(approved=True).count()
