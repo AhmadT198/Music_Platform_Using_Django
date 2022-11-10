20f108973484c34fea873ed80cf80cf3e1fd67bf
Ahmad Tamer Samir Qazamel <ahmad.qazamel@bld.ai>
2022-11-10 06:50:02 +0200

    Backend/ Create a Login Serializer.
    - Create a "loginSerializer" to check the Login input is valid.
    - Rename "UserSerializer" to "RegisterSerializer" for more convenience.
    - Override the "create" Method in RegisterSerializer to create the user automatically.
    
    ---Task 6 ----

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -7,7 +7,7 @@ from users.models import User
 
 
 
-class UserSerializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.ModelSerializer):
 
 
     username = serializers.CharField(required=True)
@@ -16,13 +16,33 @@ class UserSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(required=True)
 
     def validate(self, data): ## Overriding the Validate method for Password Validation
+        u = data.get("username",None)
+        if len(User.objects.filter(username=u)) != 0:
+            raise ValidationError("Username taken.")
+
         if data.get("password1",None) != data.get("password2",None):
             raise ValidationError("Passwords do not match.")
         else:
             validate_password(data.get("password1",None))
         return data
 
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password1'])
+        return user
 
     class Meta:
         model = User
         fields = ['email', 'username', 'password1', 'password2']
+
+
+
+class loginSerializer(serializers.ModelSerializer):
+
+
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,19 @@
+from django.utils import timezone
+from hashlib import sha512
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
+from knox.auth import TokenAuthentication
+from knox.models import AuthToken
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import mixins
+from rest_framework import mixins, permissions, status
 from rest_framework import generics
+from knox.views import LoginView as KnoxLoginView,LogoutView as KnoxLogoutView,LogoutAllView as KnoxLogoutAllView
+from django.contrib.auth.signals import user_logged_in, user_logged_out
 
 from users.models import User
 
-from .serializers import UserSerializer
+from .serializers import *
 
 
 # Create your views here.
@@ -15,14 +22,13 @@ from .serializers import UserSerializer
 class Register(APIView):
     '''Class based View to handle registeration through the endpoint /authentication/register '''
 
-
     def post(self, request, *args, **kwargs):
-        data = UserSerializer(data=request.data) ## Pass the request body to the UserSerializer
-        if data.is_valid(): ##if the data is valid.
-            user = User.objects.create_user(data.data['username'], data.data['email'].lower(), data.data['password1']) #Create the User
-            return Response({"message" : f"{user} Registered Successfully !"},status=200) ## Return Success Message
-        return Response(data.errors, status=500) ##Else Return the occuring error
-
+        data = RegisterSerializer(data=request.data)  ## Pass the request body to the UserSerializer
+        data.is_valid(raise_exception=True)  ##if the data is valid.
+        user = data.save()  # Create the User
+        return Response({
+            "message": f"{user} Registered Successfully !"
+        }, status=200)  ## Return Success Message
 
 
 ####### Example Input
