ba8f25c244eb6910072fcd74bb72cad1ab4879d4
Ahmad Tamer Samir Qazamel <ahmad.qazamel@bld.ai>
2022-11-11 16:20:34 +0200

    Backend/Create a new endpoint for manual filters.
    - Create the endpoint 'albums/manual_filters'.
    - Create 'AlbumView_ManualFilters' View that inherits from AlbumView.
    
    - Overrides the get method to use manual filtering function "manual_filter" instead of "django-filters".
    
    ---Task 8 ----

--- albums/urls.py
+++ albums/urls.py
@@ -2,7 +2,9 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', AlbumView.as_view())
+    path('', AlbumView.as_view()),
+
+    path('manual_filters', AlbumView_ManualFilters.as_view())
 
 ]
 
--- albums/views.py
+++ albums/views.py
@@ -57,3 +57,59 @@ class AlbumView(GenericAPIView):
         else:
             raise ValidationError("Not Authenticated",status.HTTP_403_FORBIDDEN)
 
+
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except:
+        return False
+
+
+
+def cost__gte(val, qs):
+    if isfloat(val):
+        return qs.filter(cost__gte=float(val))
+    else:
+        raise ValidationError("cost__gte only takes Numbers.")
+
+def cost__lte(val, qs):
+    if isfloat(val):
+        return qs.filter(cost__lte=float(val))
+    else:
+        raise ValidationError("cost__lte only takes Numbers.")
+
+def manual_filter(queryset, params):
+    if "cost__gte" in params:  ##Applying "Cost Greater Than or Equal" Filter if provided
+        queryset = cost__gte(params['cost__gte'][0], queryset)
+
+    if "cost__lte" in params:  ##Applying "Cost Less Than or Equal" Filter if provided
+        queryset = cost__lte(params['cost__lte'][0], queryset)
+
+    if "album_name" in params:  ## Applying name Filter if provided
+        p = params['album_name'][0]
+        queryset = queryset.filter(album_name__icontains=p)
+
+    return queryset
+
+class AlbumView_ManualFilters(AlbumView):
+    queryset = Album.approved_only.all()
+    serializer_class = AlbumSerializer
+
+    def get(self, request):
+        params = dict(request.query_params)
+        print(params)
+        queryset = Album.approved_only.all()
+
+
+        queryset = manual_filter(queryset, params) ##Filtering according to Existing Filters
+
+        ## Preparing Paginator
+        paginator = AlbumPagination()
+
+        ## Displaying Paginated Data
+        query = paginator.paginate_queryset(queryset= queryset , request= request)
+        data = AlbumSerializer(query, many=True)
+        return paginator.get_paginated_response(data.data)
+
+
